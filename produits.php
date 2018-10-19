6474946679979d8b5e3fdc5f97d21412938d3dc0
RoseReaper <elodie.changey@laposte.net>
2018-10-19 17:01:25 +0200

    Ajout modale

--- produits.php
+++ produits.php
@@ -39,16 +39,49 @@
       <td>---</td>
       <td>---</td>
       <td>---</td>
-      <td><a href="***"><img src="assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
+      <td><img id="modal_modif" src="assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20">
           <a href="***"><img src="assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
     </tr>
   </tbody>
 </table>
 
+<div id="modal_modif_client" class="modal">
+<div class="modal-content">
+  <div class="modal-header">
+    <span class="close">&times;</span>
+      <h2>Modal Header</h2>
+  </div>
+  <div class="modal-body">
+    <p>Some text in the Modal Body</p>
+    <p>Some other text...</p>
+  </div>
+  <div class="modal-footer">
+    <h3>Modal Footer</h3>
+  </div>
+</div>
+</div>
+
 <script type="text/javascript" src="includes/scripts/jquery-3.3.1.min.js"></script>
 
-<script type="text/javascript" src="includes/scripts/datatables.js"></script>
+<script>
+var modal = document.getElementById('modal_modif_client');
+var btn = document.getElementById("modal_modif");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+<script type="text/javascript" src="includes/scripts/datatables.js"></script>
 <script type="text/javascript">
 $(document).ready( function () {
   $('#table_clients').DataTable();
