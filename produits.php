318900b792fd878607c5dcfd27d3089ae290f38a
RoseReaper <elodie.changey@laposte.net>
2018-10-19 12:10:51 +0200

    Modif CSS

--- produits.php
+++ produits.php
@@ -7,35 +7,38 @@
 </head>
 
 <body>
-    <table>
-       <tr>
-           <th>Désignation</th>
-           <th>Référence</th>
-           <th>Prix unitaire</th>
-           <th>Actions</th>
-       </tr>
-       <tr>
-           <td>---</td>
-           <td>---</td>
-           <td>---</td>
-           <td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
-               <a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
-      </tr>
-      <tr>
-          <td>---</td>
-          <td>---</td>
-          <td>---</td>
-          <td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
-              <a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
-     </tr>
-     <tr>
-         <td>---</td>
-         <td>---</td>
-         <td>---</td>
-         <td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
-             <a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
+
+  <h1>Liste des Produits</h1>
+
+  <table>
+    <tr>
+      <th>Désignation</th>
+      <th>Référence</th>
+      <th>Prix unitaire</th>
+      <th>Actions</th>
     </tr>
-    </table>
+    <tr>
+      <td>---</td>
+      <td>---</td>
+      <td>---</td>
+      <td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
+          <a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
+    </tr>
+    <tr>
+      <td>---</td>
+      <td>---</td>
+      <td>---</td>
+      <td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
+          <a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
+    </tr>
+    <tr>
+      <td>---</td>
+      <td>---</td>
+      <td>---</td>
+      <td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
+          <a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
+    </tr>
+  </table>
 
 </body>
 </html>
