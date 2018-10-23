47d0484b3b7db5e7f58a634ad0c86cde7f51bcb1
EtiennePS <etienne.picherit@gmail.com>
2018-10-23 16:55:37 +0200

    commit de fin de journée

---
+++ View/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><? $title ?></title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="includes/style/style.css"/>
+</head>
+<body>
+<?php include('./nav.php'); ?>
+<?php include($corp) // ajout d'un ou plusieurs fichier php ?>
+</body>
+</html>
--- produits.php
+++ produits.php
@@ -1,91 +1,163 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Annuaire Produits</title>
-<link rel="stylesheet" type="text/css" href="includes/style/style.css">
-<link rel="stylesheet" type="text/css" href="includes/style/datatables.css">
-<meta charset="UTF-8">
+    <title>Annuaire Produits</title>
+    <link rel="stylesheet" type="text/css" href="includes/style/style.css">
+    <link rel="stylesheet" type="text/css" href="includes/style/datatables.css">
+    <meta charset="UTF-8">
 </head>
 
 <body>
 
+<?php include('./nav.php'); ?>
+
 <h1>Liste des Produits</h1>
 
-<table id="table_clients" class="display">
-  <thead>
+<div id="modal_ajouter_produit" class="modal">
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2>Ajouter un produit</h2>
+        </div>
+        <div class="modal-body">
+            <p>Some text in the Modal Body</p>
+            <p>Some other text...</p>
+            <button type="button">Valider</button>
+            <button type="button">Annuler</button>
+        </div>
+    </div>
+</div>
+
+<button id="bouton_ajouter" type="button">Ajouter un nouveau produit</button>
+
+<div id="modal_modif_produit" class="modal">
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2>Modifier la Fiche produit</h2>
+        </div>
+        <div class="modal-body">
+            <p>Some text in the Modal Body</p>
+            <p>Some other text...</p>
+            <button type="button">Valider</button>
+            <button type="button">Annuler</button>
+        </div>
+    </div>
+</div>
+
+<table id="table_produits" class="display">
+    <thead>
     <tr>
-      <th>Désignation</th>
-      <th>Référence</th>
-      <th>Prix unitaire</th>
-      <th>Actions</th>
+        <th>Désignation</th>
+        <th>Référence</th>
+        <th>Prix unitaire</th>
+        <th>Actions</th>
     </tr>
-  </thead>
-  <tbody>
+    </thead>
+    <tbody>
     <tr>
-      <td>---</td>
-      <td>---</td>
-      <td>---</td>
-      <td><a href="***"><img src="assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
-          <a href="***"><img src="assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
-    </tr>
-    <tr>
-      <td>---</td>
-      <td>---</td>
-      <td>---</td>
-      <td><a href="***"><img src="assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
-          <a href="***"><img src="assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
+        <td>---</td>
+        <td>---</td>
+        <td>---</td>
+        <td><img class="modal_modif" src="assets/pencil.png" title="Modifier Profil produit" alt="bouton_modifier" height="20">
+            <a href="#"><img src="assets/cancel.png" title="Supprimer Profil produit" alt="bouton_supprimer" height="20"/></a></td>
     </tr>
     <tr>
-      <td>---</td>
-      <td>---</td>
-      <td>---</td>
-      <td><img id="modal_modif" src="assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20">
-          <a href="***"><img src="assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
+        <td>---</td>
+        <td>---</td>
+        <td>---</td>
+        <td><img class="modal_modif" src="assets/pencil.png" title="Modifier Profil produit" alt="bouton_modifier" height="20">
+            <a href="#"><img src="assets/cancel.png" title="Supprimer Profil produit" alt="bouton_supprimer" height="20"/></a></td>
     </tr>
-  </tbody>
+    <tr>
+        <td>---</td>
+        <td>---</td>
+        <td>---</td>
+        <td><img class="modal_modif" src="assets/pencil.png" title="Modifier Profil produit" alt="bouton_modifier" height="20">
+            <a href="#"><img src="assets/cancel.png" title="Supprimer Profil produit" alt="bouton_supprimer" height="20"/></a></td>
+    </tr>
+    </tbody>
 </table>
 
-<div id="modal_modif_client" class="modal">
-<div class="modal-content">
-  <div class="modal-header">
-    <span class="close">&times;</span>
-      <h2>Modal Header</h2>
-  </div>
-  <div class="modal-body">
-    <p>Some text in the Modal Body</p>
-    <p>Some other text...</p>
-  </div>
-  <div class="modal-footer">
-    <h3>Modal Footer</h3>
-  </div>
-</div>
-</div>
-
 <script type="text/javascript" src="includes/scripts/jquery-3.3.1.min.js"></script>
-
-<script>
-var modal = document.getElementById('modal_modif_client');
-var btn = document.getElementById("modal_modif");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-
 <script type="text/javascript" src="includes/scripts/datatables.js"></script>
+
 <script type="text/javascript">
-$(document).ready( function () {
-  $('#table_clients').DataTable();
-} );
+
+    var modalModif = document.getElementById("modal_modif_produit");
+    for(var i=0; i<btnModif.length; i++)
+    {
+        btnModif[i].onclick = function()
+        {
+            modalModif.style.display = "block";
+        }
+    }
+    var spanModif = document.getElementsByClassName("close")[0];
+
+    spanModif.onclick = function()
+    {
+        modalModif.style.display = "none";
+    }
+
+    window.onclick = function(event)
+    {
+        if (event.target == modalAjout)
+        {
+            modalModif.style.display = "none";
+        }
+    }
+
+    var modalAjout = document.getElementById("modal_ajouter_produit");
+    var btnAjout = document.getElementById("bouton_ajouter");
+    btnAjout.onclick = function() {
+        modalAjout.style.display = "block";
+    }
+
+    var spanAjout = document.getElementsByClassName("close")[0];
+
+    spanAjout.onclick = function()
+    {
+        modalAjout.style.display = "none";
+    }
+    window.onclick = function(event)
+    {
+        if (event.target == modalAjout)
+        {
+            modalAjout.style.display = "none";
+        }
+    }
+
+    $(document).ready( function ()
+    {
+        $('#table_produits').DataTable();
+    } );
+
+    $('#table_produits').DataTable(
+    {
+        language: {
+            processing:     "Traitement en cours...",
+            search:         "Rechercher&nbsp;:",
+            lengthMenu:    "Afficher _MENU_ &eacute;l&eacute;ments",
+            info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            infoPostFix:    "",
+            loadingRecords: "Chargement en cours...",
+            zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+            emptyTable:     "Aucune donnée disponible dans le tableau",
+            paginate: {
+                first:      "Premier",
+                previous:   "Pr&eacute;c&eacute;dent",
+                next:       "Suivant",
+                last:       "Dernier"
+            },
+            aria: {
+                sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        }
+    } );
+
 </script>
 
 </body>
