d3c4e951a74f3fe6097a0cb7a2e4132100dce22e
maxime173 <17.laurinmaxime@gmail.com>
2018-10-24 14:12:02 +0200

    test

---
+++ Model/model.php
@@ -0,0 +1,10 @@
+<?php
+// include 'bdd.php'; // TODO seul PDO en global, fonction init pour l'instancier
+include 'categorie-produit.php';
+include 'gamme.php';
+include 'marque.php';
+include 'produit.php';
+include 'TVA.php';
+include 'ligneBonLivraison.php';
+include 'ligneDevis.php';
+include 'ligneFacture.php';
