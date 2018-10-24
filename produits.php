71bbd129f91ba6c2f145ca1a331bce1a04b272af
RoseReaper <elodie.changey@laposte.net>
2018-10-24 15:16:37 +0200

    Formulaire ajout produits

--- produits.php
+++ produits.php
@@ -20,8 +20,37 @@
     <h2>Ajouter un produit</h2>
   </div>
     <div class="modal-body">
-      <p>Some text in the Modal Body</p>
-      <p>Some other text...</p>
+      <form onsubmit="return foncverif()">
+        <p><label for="libellé_produit">Libellé :</label>
+        <input type="text" id="libellé_produit" name="libellé_produit"></p>
+
+        <p><label for="marque_produit">Marque :</label>
+        <select name="marque_produit" id="marque_produit">
+          <option value="none">--</option>
+        </select></p>
+
+        <p><label for="gamme_produit">Gamme :</label>
+        <select name="gamme_produit" id="gamme_produit">
+          <option value="none">--</option>
+        </select></p>
+
+        <p><label for="référence_produit">Référence :</label>
+        <input type="text" id="référence_produit" name="référence_produit"></p>
+
+        <p><label for="prixht_produit">Prix (HT) :</label>
+        <input type="text" id="prixht_produit" name="prixht_produit"></p>
+
+        <p><label for="tva_produit">TVA :</label>
+        <select name="tva_produit" id="tva_produit">
+          <option value="none">--</option>
+        </select></p>
+
+        <p><label for="quantité_produit">Quantité :</label>
+        <input type="text" id="quantité_produit" name="quantité_produit"></p>
+
+        <p>Actif :<br>
+        <input type="checkbox" name="inactif" id="inactif" /> <label for="inactif">Inactif</label></p>
+
        <button type="button">Valider</button>
        <button type="button">Annuler</button>
     </div>
