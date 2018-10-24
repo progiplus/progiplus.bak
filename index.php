18fdd83de4b06c1527f8fe223b9a5089ff6e5f44
RoseReaper <elodie.changey@laposte.net>
2018-10-24 14:19:14 +0200

    Ajout index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'Model/model.php';
+require_once 'Controller/abstractController.php';
+require_once 'Controller/clientController.php';
+require_once 'Controller/produitController.php';
+
+function main()
+{
+    $page = isset($_GET["page"]) ? $_GET["page"] : null;
+    $action = isset($_GET["action"]) ? $_GET["action"] : null;
+
+    switch($page)
+    {
+        case "client":
+            $controller = new clientController();
+            break;
+        case "produit":
+            $controller = new produitController();
+            break;
+        case null:
+        default:
+            require("accueil.php"); // plutôt accueil du site
+            return null;
+            break;
+    }
+    return $controller->menu($action);
+}
+
+echo main();
