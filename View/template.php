c5de4db2c6406c32e0123207de81c534a375cc03
EtiennePS <etienne.picherit@gmail.com>
2018-10-25 11:56:30 +0200

    ajout favicon

--- View/template.php
+++ View/template.php
@@ -4,6 +4,7 @@
     <title><?php echo $title ?></title>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="includes/style/style.css"/>
+    <link rel="icon" href="assets/favicon.ico" />
 </head>
 <body>
 <?php include('./nav.php'); ?>
