d937da70dd27e80e6a3ab439bcba3da100d55d4b
gonzo47 <gonzalez.benjamin47@gmail.com>
2018-10-26 15:42:22 +0200

    commit

--- client.php
+++ client.php
@@ -1,104 +1,46 @@
-<!DOCTYPE html>
+<section>
+    <h1>Annuaire Client <button>Ajouter client</button></h1>
 
-<head>
-	<link rel="stylesheet" type="text/css" href="includes/style/style.css" >
-<<<<<<< HEAD
-    <link rel="stylesheet" type="text/css" href="includes/style/datatables.css" >
-    
-=======
-	<link rel="stylesheet" type="text/css" href="includes/style/datatables.css">
->>>>>>> elodie
-	<title> Progiplus-client</title>
-	</head>
-<nav>
-
-</nav>
-
-
-<body>
-    <?php include('./nav.php'); ?>
-	<h1>Annuaire Client</h1>
-
-	<!-- version vanilla <table >
-
-		<tr>
-			<th>Code client</th>
-			<th>raison sociale</th>
-			<th>Nom contact</th>
-			<th>prenom de contact</th>
-			<th>service de contact</th>
-			<th>moyenn de communication</th>
-			<th>Actions</th>
-
-			</tr>
-			<td>---</td>
-			<td>---</td>
-			<td>---</td>
-			<td>---</td>
-			<td>---</td>
-			<td>---</td>
-			<td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
-               <a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
-		</td>
-	</table> -->
-
-
-
-	<table id="table_client" class="display">
+    <table id="table_client" class="display">
         <thead>
-	        <tr>
-				<th>Code client</th>
-				<th>Raison sociale</th>
-				<th>Nom contact</th>
-				<th>Prénom de contact</th>
-				<th>Service de contact</th>
-				<th>Moyen de communication</th>
-				<th>Actions</th>
-                </thead>
-			</tr>
-<<<<<<< HEAD
+            <tr>
+                <th>Code client</th>
+                <th>Raison sociale</th>
+                <th>Civilite</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
     <tbody>
-			<?php 
-=======
-			<?php
->>>>>>> elodie
-			for($row = 0; $row < 15; $row ++ )
-			{
-				echo '<tr>';
-						for($col = 0; $col < 6; $col ++ )
-						{
-							echo '<td>col</td>';
-						}
+    <?php 
 
-				echo '
-				<td><a href="***"><img src="./assets/pencil.png" title="Modifier Profil client" alt="bouton_modifier" height="20"/></a>
-					<a href="***"><img src="./assets/cancel.png" title="Supprimer Profil client" alt="bouton_supprimer" height="20"/></a></td>
-				</td>
+        while($clients = $data->fetch())
+        {
+            echo '<tr>';
+                echo '<td>' . $clients['code_client'] . '</td>';
+                echo '<td>' . $clients['raison_sociale'] . '</td>';
+                echo '<td>' . $clients['libelle'] . '</td>';
+                echo '<td>' . $clients['nom'] . '</td>';
+                echo '<td>' . $clients['prenom'] . '</td>';
 
-				</tr>';
-            }
-        
-           
-			
-			?>
+                echo '<td><a href="#"><img src="./assets/pencil.png" class="imageTableau" title="Modifier Profil client" alt="bouton_modifier"/></a>
+                   <a href="#"><img src="./assets/cancel.png" class="imageTableau" title="Supprimer Profil client" alt="bouton_supprimer"/></a>
+                </td>';
+            echo '</tr>';
+        }
+        Database::disconnect();
+    ?>
         </tbody>
+    </table>
 
-</table>
-    <script type="text/javascript"  src="includes/scripts/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript"  src="includes/scripts/datatables.js"></script>
+</section>
+<script type="text/javascript"  src="includes/scripts/jquery-3.3.1.min.js"></script>
+<script type="text/javascript"  src="includes/scripts/datatables.js"></script>
 
-    <script type="text/javascript">
-     $(document).ready( function () {
-            $('#table_client').DataTable();
-            } );
-  
+<script type="text/javascript">
+    $(document).ready( function () {
+        $('#table_client').DataTable();
+        } );
+</script>
 
-    </script>
-
-    
-    
-</body>
-
-<footer></footer>
-
-</html>
--- nav.php
+++ nav.php
@@ -1,41 +1,41 @@
-<nav>
-    <img id="logo" src="includes/media/logo.jpg" width="200px">
+<nav id="menu">
+    <img id="logo" src="includes/media/logo.jpg">
     <form id="rechercher">
         <input type="text" placeholder="Rechercher..">
-        <button type="submit"><img alt="" src="includes/media/Search-icon.png" width="30px"></button>
+        <button type="submit"><img src="includes/media/Search-icon.png" class="imageRechercher" alt="echercher"></button>
     </form>
-    <div id="menu">
-        <ul>
-            <li><a href="#">Accueil</a></li>
+    <ul>
+        <li><a href="#">Accueil</a></li>
 
-            <li><a href="#">Annuaire Client</a>
-                <ul>
-                    <li><a href="#">Ajouter un client</a></li>
-                    <li><a href="#">Modifier un client </a></li>
-                </ul>
-            </li>
+        <li><a href="#">Annuaire Client</a>
+            <ul>
+                <li><a href="#">Liste client</a></li>
+                <li><a href="#">Ajouter un client</a></li>
+                <li><a href="#">Modifier un client </a></li>
+            </ul>
+        </li>
 
-            <li><a href="#">Annuaire Fournisseur</a>
-                <ul>
-                    <li><a href="#">Ajouter un fournisseur</a></li>
-                    <li><a href="#">Modifier un fournisseur </a></li>
-                </ul>
-            </li>
+        <li><a href="#">Produits</a>
+            <ul>
+                <li><a href="#">Liste produit</a></li>
+                <li><a href="#">Ajouter un produit</a></li>
+                <li><a href="#">Modifier un produit </a></li>
+            </ul>
+        </li>
 
-            <li><a href="#">Liste Produit</a>
-                <ul>
-                    <li><a href="#">Ajouter un produit</a></li>
-                    <li><a href="#">Modifier un produit </a></li>
-                </ul>
-            </li>
-
-            <li><a href="#">Documents</a>
-                <ul>
-                    <li><a href="#">Devis</a></li>
-                    <li><a href="#">Facture</a></li>
-                    <li><a href="#">Bon de commande</a></li>
-                </ul>
-            </li>
-        </ul>
-    </div>
+        <li><a href="#">Devis</a>
+			<ul>
+				<li><a href="#">Nouveau devis</a></li>
+				<li><a href="#">Modifier devis</a></li>
+			</ul>
+		</li>
+                	
+		<li><a href="#">Facture</a>
+			<ul>
+				<li><a href="#">Nouvelle facture</a></li>
+				<li><a href="#">Modifier facture</a></li>
+			</ul>
+        </li>
+        <li><a href="#">Bon de livraison</a></li>
+    </ul>
 </nav>
