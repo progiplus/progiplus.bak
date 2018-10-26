1ff530cfc20ed9a019d9cd201bbd19e4276691cd
EtiennePS <etienne.picherit@gmail.com>
2018-10-26 14:27:56 +0200

    màj acceuil.php

--- accueil.php
+++ accueil.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Acceuil</title>
-		<meta charset="utf-8"/>
-			<link rel="stylesheet" href="includes/style/style.css"/>
-	</head>
-    <body>
-        <?php include('./nav.php'); ?>
-    </body>
-</html>
+<section>
+    
+</section>
