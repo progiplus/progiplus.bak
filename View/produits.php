7f60652bd81e2930ff89e2e6c4a0486dc5357ee5
RoseReaper <elodie.changey@laposte.net>
2018-10-30 09:41:31 +0100

    Modif CSS bouton ajouter produit + langue sur Database produit

--- View/produits.php
+++ View/produits.php
@@ -37,8 +37,8 @@
         <p><label for="quantité_produit">Quantité :</label>
         <input type="text" id="quantité_produit" name="quantité_produit"></p>
 
-        <p>Actif :<br>
-        <input type="checkbox" name="inactif" id="inactif" /> <label for="inactif">Inactif</label></p>
+        <p>Cocher cette case pour désactiver le produit :<br>
+        <input type="checkbox" name="inactif" id="inactif" /> <label for="inactif"></label></p>
 
        <button type="button">Valider</button>
        <button type="button">Annuler</button>
@@ -143,33 +143,8 @@ function foncAjoutMarque(){
   }
 
 $(document).ready( function () {
-  $('#table_produits').DataTable();
+  $('#table_produits').DataTable({"language": getLangageDataTable("produit", false)});
 } );
-
-$('#table_produits').DataTable( {
-    language: {
-        processing:     "Traitement en cours...",
-        search:         "Rechercher&nbsp;:",
-        lengthMenu:    "Afficher _MENU_ &eacute;l&eacute;ments",
-        info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-        infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
-        infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-        infoPostFix:    "",
-        loadingRecords: "Chargement en cours...",
-        zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
-        emptyTable:     "Aucune donnée disponible dans le tableau",
-        paginate: {
-            first:      "Premier",
-            previous:   "Pr&eacute;c&eacute;dent",
-            next:       "Suivant",
-            last:       "Dernier"
-        },
-        aria: {
-            sortAscending:  ": activer pour trier la colonne par ordre croissant",
-            sortDescending: ": activer pour trier la colonne par ordre décroissant"
-        }
-    }
-} );
-
 </script>
+
 </section>
